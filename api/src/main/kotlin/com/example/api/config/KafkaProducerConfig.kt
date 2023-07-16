46054bfab162d12137be300df57dc45b295113db
yongckim <zxcv9203@naver.com>
2023-07-17 00:30:59 +0900

    :sparkles: 카프카를 통해 쿠폰 생성을 프로듀스 하도록 변경

---
+++ api/src/main/kotlin/com/example/api/config/KafkaProducerConfig.kt
@@ -0,0 +1,30 @@
+package com.example.api.config
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.LongSerializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Long> {
+        val config = HashMap<String, Any>()
+
+        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = LongSerializer::class.java
+
+        return DefaultKafkaProducerFactory(config)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Long> {
+        return KafkaTemplate(producerFactory())
+    }
+}
--- api/src/main/kotlin/com/example/api/service/ApplyService.kt
+++ api/src/main/kotlin/com/example/api/service/ApplyService.kt
@@ -1,6 +1,6 @@
 package com.example.api.service
 
-import com.example.api.domain.Coupon
+import com.example.api.producer.CouponCreateProducer
 import com.example.api.repository.CouponCountRepository
 import com.example.api.repository.CouponRepository
 import org.springframework.stereotype.Service
@@ -8,17 +8,16 @@ import org.springframework.stereotype.Service
 @Service
 class ApplyService(
     private val couponRepository: CouponRepository,
-    private val couponCountRepository: CouponCountRepository
+    private val couponCountRepository: CouponCountRepository,
+    private val couponCreateProducer: CouponCreateProducer
 ) {
     fun apply(userId: Long) {
-        val count = couponCountRepository.increment()?: 100L
+        val count = couponCountRepository.increment() ?: 100L
 
         if (count > 100) {
             return
         }
 
-        couponRepository.save(
-            Coupon(userId = userId)
-        )
+        couponCreateProducer.create(userId)
     }
 }
